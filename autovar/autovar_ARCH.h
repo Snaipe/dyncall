74ccdb6f87799e8222f52c7463b0772816a64378
cslag <unknown>
2015-11-27 01:18:22 +0100

    - ported _macros.h change from 2 commits ago to autovar (which will replace former, in 1.0)

--- autovar/autovar_ARCH.h
+++ autovar/autovar_ARCH.h
@@ -27,10 +27,10 @@
 #define AUTOVAR_ARCH_H
 
 /* Check architecture. */
-#if defined(_M_IX86) || defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__) || defined(__386__) || defined(__i386)
-# define ARCH_X86
-#elif defined(_M_X64_) || defined(_M_AMD64) || defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) 
+#if defined(_M_X64_) || defined(_M_AMD64) || defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) 
 # define ARCH_X64
+#elif defined(_M_IX86) || defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__) || defined(__386__) || defined(__i386)
+# define ARCH_X86
 #elif defined(_M_IA64) || defined(__ia64__)
 # define ARCH_IA64
 #elif defined(_M_PPC) || defined(__powerpc__) || defined(__powerpc) || defined(__POWERPC__) || defined(__ppc__) || defined(__power__)
